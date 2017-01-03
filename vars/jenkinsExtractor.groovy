0046ab8f167ac6f3bea3f99707823754e4d0ff2f
Andrey Devyatkin <ady@praqma.net>
2017-01-03 14:17:22 +0100

    Fix #2: Create data class to describe build data (#11)

--- vars/jenkinsExtractor.groovy
+++ vars/jenkinsExtractor.groovy
@@ -1,9 +1,9 @@
-// This method takes a list of job names and a time interval in hours
-def call(def jobNames, def numberOfHoursBack) {
-	
-	// Build list of jobs
-	def buildResults = []
+import net.praqma.quticon.BuildDataEntry;
 
+// This method takes a list of job names and a time interval in hours
+// and returns list of BuildDataEntry
+def call(def jobNames, def numberOfHoursBack) {
+	def buildResults = []
     	for (def jobName: jobNames) {
 		echo "Looking for the job with the name $jobName"	
 		job = Jenkins.instance.getItem(jobName)
@@ -13,13 +13,9 @@ def call(def jobNames, def numberOfHoursBack) {
     		def builds = job.getBuilds().byTimestamp(System.currentTimeMillis()-numberOfHoursBack*60*60*1000, System.currentTimeMillis())
     		echo "Found ${builds.size()} builds matching time criteria for the job ${jobName}"
 		for (def build: builds) {
-        		def buildResult =[:]
-        		buildResult["name"] = job.name
-        		buildResult["result"] = build.result
-            		buildResult["duration"] = build.duration
-            		buildResult["timestamp"] = build.getTimestamp().getTimeInMillis()
-			echo "Name ${job.name}, result ${build.result}, duration ${build.duration}, timestamp ${build.getTimestamp().getTimeInMillis()}"
-            		buildResults.add(buildResult)
+			entry = new BuildDataEntry(jobName: jobName, verdict: build.result, duration: build.duration, timestamp: build.getTimestamp().getTimeInMillis())
+			echo "Name ${entry.jobName}, result ${entry.verdict}, duration ${entry.duration}, timestamp ${entry.timestamp}"
+            		buildResults.add(entry)
         	}
 	}
 	return buildResults

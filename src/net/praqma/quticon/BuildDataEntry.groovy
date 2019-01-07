7ed483d3f76a69e813d0cb7af9b878029a5fb8de
tooextlb <lars.berntzon@external.atlascopco.com>
2019-01-07 17:17:58 +0100

    Include jobname in the json
    Change-Id: Ifd5db1b44a50ed1a17378dadc00d4f6660921731

--- src/net/praqma/quticon/BuildDataEntry.groovy
+++ src/net/praqma/quticon/BuildDataEntry.groovy
@@ -1,3 +1,11 @@
 package net.praqma.quticon
 
-class BuildDataEntry { String job_name; String verdict; int build_number; long duration; long timestamp; long time_in_queue }
+class BuildDataEntry
+{
+    String job_name;
+    String verdict;
+    int build_number;
+    long duration;
+    long timestamp;
+    long time_in_queue;
+}
--- vars/jenkinsExtractor.groovy
+++ vars/jenkinsExtractor.groovy
@@ -9,47 +9,53 @@ import com.cloudbees.workflow.rest.external.RunExt
 // NonCPS added to avoid java.io.NotSerializableException's
 // Read more here https://github.com/jenkinsci/workflow-cps-plugin#technical-design
 @NonCPS
-def call(def jobNames, def numberOfHoursBack) {
-	def buildResults = []
-	if (jobNames==[]) {
-		echo "No job names specified for extraction. Check all jobs"
-		jobNames = Jenkins.instance.getJobNames()
-	}
-    	for (def jobName: jobNames) {
-		echo "Looking for the job with the name $jobName"
-		def job = Jenkins.instance.getItemByFullName(jobName)
-  		if (job == null) {
-          		echo "Job ${jobName} wasn't found. Check your configuration"
-			continue
-        	}
-		def builds = job.getBuilds().byTimestamp(System.currentTimeMillis()-numberOfHoursBack*60*60*1000, System.currentTimeMillis()).completedOnly()
-    		echo "Found ${builds.size()} builds matching time criteria for the job ${jobName}"
-		for (def build: builds) {
-			// Make sure we remove / from job name since it will be part of URL and might confuse ELK
-			sanitizedJobName = jobName.replaceAll("/", ".")
-			// If this is a pipeline then extract pipeline stages as separate entries in addition to the pipeline run itself that will
-			// be extracted in the next step
-			if (build instanceof WorkflowRun) {
-				for (flowNode in RunExt.create(build).getStages()) {
-					def stage_entry = new BuildDataEntry(job_name: "${sanitizedJobName}.${flowNode.getName().replaceAll("[^a-zA-Z0-9]", "_")}", 
-			           		   verdict: flowNode.getStatus(),
-			                           build_number: build.number,
-				                   duration: flowNode.getDurationMillis(), 
-				                   timestamp: flowNode.getStartTimeMillis(),
-						   time_in_queue: 0) // Fixme!
-					echo "New pipeline stage entry: name ${stage_entry.job_name}, result ${stage_entry.verdict}, number ${stage_entry.build_number}, duration ${stage_entry.duration}, timestamp ${stage_entry.timestamp}, time in queue ${stage_entry.time_in_queue}"
-            				buildResults.add(stage_entry)
-				}
-			}
-			 def entry = new BuildDataEntry(job_name: sanitizedJobName, 
-			           		   verdict: build.result,
-			                           build_number: build.number,
-				                   duration: build.duration, 
-				                   timestamp: build.getTimeInMillis(),
-						   time_in_queue: build.getStartTimeInMillis() - build.getTimeInMillis())
-			echo "New entry: name ${entry.job_name}, result ${entry.verdict}, number ${entry.build_number}, duration ${entry.duration}, timestamp ${entry.timestamp}, time in queue ${entry.time_in_queue}"
-            		buildResults.add(entry)
-        	}
-	}
-	return buildResults
+def call(def jobNames, def numberOfHoursBack)
+{
+    def buildResults = []
+
+    if (jobNames==[]) {
+        echo "No job names specified for extraction. Check all jobs"
+        jobNames = Jenkins.instance.getJobNames()
+    }
+
+    for (def jobName: jobNames) {
+        echo "Looking for the job with the name $jobName"
+        def job = Jenkins.instance.getItemByFullName(jobName)
+        if (job == null) {
+            echo "Job ${jobName} wasn't found. Check your configuration"
+            continue
+        }
+
+        def builds = job.getBuilds().byTimestamp(System.currentTimeMillis()-numberOfHoursBack*60*60*1000, System.currentTimeMillis()).completedOnly()
+        echo "Found ${builds.size()} builds matching time criteria for the job ${jobName}"
+        for (def build: builds) {
+            // If this is a pipeline then extract pipeline stages as separate entries in addition to the pipeline run itself that will
+            // be extracted in the next step
+            if (build instanceof WorkflowRun) {
+                for (flowNode in RunExt.create(build).getStages()) {
+                    def stage_entry = new BuildDataEntry(
+                               job_name: "${jobName}.${flowNode.getName()}", 
+                               verdict: flowNode.getStatus(),
+                               build_number: build.number,
+                               duration: flowNode.getDurationMillis(), 
+                               timestamp: flowNode.getStartTimeMillis(),
+                               time_in_queue: 0) // Fixme!
+                    echo "New pipeline stage entry: name ${stage_entry.job_name}, result ${stage_entry.verdict}, number ${stage_entry.build_number}, duration ${stage_entry.duration}, timestamp ${stage_entry.timestamp}, time in queue ${stage_entry.time_in_queue}"
+                    buildResults.add(stage_entry)
+                }
+            }
+            def entry = new BuildDataEntry(
+                               job_name: jobName, 
+                               verdict: build.result,
+                               build_number: build.number,
+                               duration: build.duration, 
+                               timestamp: build.getTimeInMillis(),
+                               time_in_queue: build.getStartTimeInMillis() - build.getTimeInMillis()
+            )
+            echo "New entry: name ${entry.job_name}, result ${entry.verdict}, number ${entry.build_number}, duration ${entry.duration}, timestamp ${entry.timestamp}, time in queue ${entry.time_in_queue}"
+            buildResults.add(entry)
+        }
+    }
+
+    return buildResults
 }

75bd7758f1fbab8a1d3602cd522e59fe06331634
tooextlb <lars.berntzon@external.atlascopco.com>
2019-10-04 13:37:50 +0200

    modified:   vars/jenkinsExtractor.groovy
    Change-Id: I42f618aa0807f6acf1a456cecfb5e0d87dfb9736

--- vars/jenkinsExtractor.groovy
+++ vars/jenkinsExtractor.groovy
@@ -58,7 +58,13 @@ def call(def jobNames=[], def numberOfHoursBack=24, def excludedJobs=[])
                                time_in_queue: build.getStartTimeInMillis() - build.getTimeInMillis(),
                                entry_type: "build",
                                description: build.description)
-            echo "New entry: name ${entry.job_name}, result ${entry.verdict}, number ${entry.build_number}, duration ${entry.duration}, timestamp ${entry.timestamp}, time in queue ${entry.time_in_queue}"
+            echo "New entry: name->${entry.job_name}, " +
+                 "result->${entry.verdict}, " +
+                 "number->${entry.build_number}, " +
+                 "duration->${entry.duration}, " +
+                 "timestamp->${entry.timestamp}, " +
+                 "time in queue->${entry.time_in_queue}" +
+                 "description->${entry.description}"
             buildResults.add(entry)
         }
     }
